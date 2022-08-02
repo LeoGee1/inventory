590a09d6a29c1d26fcfa58c2b4e0d9c8f2d3b9c3
LeoGee1 <ucheleonard2000@gmail.com>
2022-08-02 23:38:31 +0100

    done with the feature

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-const list = document.querySelector('#product-info1')
-const items = document.querySelectorAll('#ballers')
-const cancel = document.querySelectorAll('.fa-xmark')
-const ballWord = document.querySelectorAll('.word')
+const list = document.querySelector('#product-info1');
+const items = document.querySelectorAll('#ballers');
+const cancel = document.querySelectorAll('.fa-xmark');
+const ballWord = document.querySelectorAll('.word');
 
 // showing the hover X button *hahahahaha*
-list.addEventListener('mouseover', function(e){
-    if(items && e.target.className === 'word'){
-        e.target.nextElementSibling.style.visibility = 'visible'
-    }
-})
+// list.addEventListener('mouseover', function(e){
+//     if(items && e.target.className === 'word'){
+//         e.target.nextElementSibling.style.visibility = 'visible'
+//     }
+// })
    
 // removing it when not more on hover
 
@@ -34,20 +34,177 @@ page.addEventListener('click', function(e){
 })
 
 // adding elements to the html
-const addball = document.querySelectorAll('.plus')
+
+const addballs = document.forms['product-add1'] 
+// greenball
+addballs.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const value = document.querySelector('input[type="text"]').value.trim()
+    if (value.length === 0 || value.length > 11) {
+        return
+    }else if (value !== 'green ball') {
+        return
+    }
+    
+    // create green elements
+    const greenball = document.createElement('div');
+    const ballname = document.createElement('h3');
+    const deleteball = document.createElement('i');
 
 
+    //add text contents
+    ballname.textContent = value
+    // hv.textContent = 'high value'
+
+    //adding classes
+    greenball.classList.add('balls', 'one');
+    deleteball.classList.add('fa-solid', 'fa-xmark')
+
+   
+    //append each to each
+
+    greenball.appendChild(ballname);
+    greenball.appendChild(deleteball);
+    list.appendChild(greenball);
+    // e.target.reset
+})
+
+// white balls
+addballs.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const value = document.querySelector('input[type="text"]').value.trim()
+    if (value.length === 0 || value.length > 11) {
+        return
+    }else if (value !== 'white ball') {
+        return
+    }
+    
+    // create white elements
+    const whiteball = document.createElement('div');
+    const ballname = document.createElement('h3');
+    const deleteball = document.createElement('i');
 
 
+    //add text contents
+    ballname.textContent = value
+    // hv.textContent = 'high value'
+
+    //adding classes
+    whiteball.classList.add('balls', 'two');
+    deleteball.classList.add('fa-solid', 'fa-xmark')
+
+   
+    //append each to each
+
+    whiteball.appendChild(ballname);
+    whiteball.appendChild(deleteball);
+    list.appendChild(whiteball);
+    // e.target.reset
+})
+
+//basketballs
+addballs.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const value = document.querySelector('input[type="text"]').value.trim()
+    if (value.length === 0 || value.length > 11) {
+        return
+    }else if (value !== 'basketball') {
+        return
+    }
+    
+    // create white elements
+    const basketball = document.createElement('div');
+    const ballname = document.createElement('h3');
+    const deleteball = document.createElement('i');
+    const hv = document.createElement('h4');
+
+    //add text contents
+    ballname.textContent = value
+    hv.textContent = 'high value'
+
+    //adding classes
+    basketball.classList.add('balls', 'three');
+    deleteball.classList.add('fa-solid', 'fa-xmark', 'basket')
+    hv.classList.add('high')
+
+    //append each to each
+    basketball.appendChild(ballname);
+    basketball.appendChild(deleteball);
+    basketball.appendChild(hv)
+    list.appendChild(basketball);
+    // e.target.reset
+})
+
+//tennis ball
+addballs.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const value = document.querySelector('input[type="text"]').value.trim()
+    if (value.length === 0 || value.length > 11) {
+        return
+    }else if (value !== 'tennis ball') {
+        return
+    }
+    
+    // create white elements
+    const tennisball = document.createElement('div');
+    const ballname = document.createElement('h3');
+    const deleteball = document.createElement('i');
 
 
+    //add text contents
+    ballname.textContent = value
+    // hv.textContent = 'high value'
+
+    //adding classes
+    tennisball.classList.add('balls', 'four');
+    deleteball.classList.add('fa-solid', 'fa-xmark')
+
+   
+    //append each to each
+
+    tennisball.appendChild(ballname);
+    tennisball.appendChild(deleteball);
+    list.appendChild(tennisball);
+    // e.target.reset
+})
+
+//baseball
+addballs.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    const value = document.querySelector('input[type="text"]').value.trim()
+    if (value.length === 0 || value.length > 11) {
+        return
+    }else if (value !== 'baseball') {
+        return
+    }
+    
+    // create white elements
+    const baseball = document.createElement('div');
+    const ballname = document.createElement('h3');
+    const deleteball = document.createElement('i');
 
 
+    //add text contents
+    ballname.textContent = value
+    // hv.textContent = 'high value'
 
+    //adding classes
+    baseball.classList.add('balls', 'five');
+    deleteball.classList.add('fa-solid', 'fa-xmark')
 
+   
+    //append each to each
 
-
-
+    baseball.appendChild(ballname);
+    baseball.appendChild(deleteball);
+    list.appendChild(baseball);
+    // e.target.reset
+})
 
 
 
